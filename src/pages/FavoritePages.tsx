d1692a032f7d91c67f8006c0cbdc0ea738731817
JDGB1428 <juangalindo28@hotmail.com>
2024-12-24 12:21:44 -0500

    Creacion de pages

---
+++ src/pages/FavoritePages.tsx
@@ -0,0 +1,7 @@
+
+
+export default function FavoritePages() {
+    return (
+        <div>FavoritePages</div>
+    )
+}
---
+++ src/pages/IndexPages.tsx
@@ -0,0 +1,7 @@
+
+
+export default function IndexPages() {
+    return (
+        <div>IndexPages</div>
+    )
+}
